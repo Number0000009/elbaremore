84602da6713ce70951c0f5b43e2236c73914a442
motherf <no.0000000000009@gmail.com>
2019-02-19 10:00:01 +0000

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+WORKDIR=$(builtin cd $(dirname $0) && pwd)
+
+[[ -n ${MODEL} ]] || MODEL=mysupermodel
+[[ -n ${IMAGE} ]] || IMAGE=${WORKDIR}/main
+[[ -n ${CPUS} ]] || CPUS=4
+[[ ${CPUS} < 1 ]] && CPUS=1
+
+MAXCORE=$((CPUS-1))
+
+cmd="${MODEL} \
+	-S \
+	-R \
+	-C cluster0.NUM_CORES=${CPUS} \
+	-C cache_state_modelled=0 \
+	-C bp.pl011_uart0.uart_enable=1 \
+	$(eval for i in \{0..${MAXCORE}\}\; do echo '-a 'cluster0.cpu\$i=${IMAGE}\; done)"
+
+	$cmd
